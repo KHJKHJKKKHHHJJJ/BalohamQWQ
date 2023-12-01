eb08327470743a63f92dde2c890ce0d4ccb96e80
KHJKHJKKKHHHJJJ <137582646+KHJKHJKKKHHHJJJ@users.noreply.github.com>
2023-12-01 20:45:26 +0900

    annotated

--- selectDay.py
+++ selectDay.py
@@ -4,8 +4,11 @@ import flashCard as fC
 import quiz as qz
 from colors import bg_center, btncol
 
+# 클래스 정의
 class DateSelWin:
+    # 초기 매개 변수: 창, s냐 아님 q냐, 유저 ID)
     def __init__(self, dateSel_window, sorq, Id):
+        # 창 셀프화
         self.dateSel_window = dateSel_window
         self.dateSel_window.title("Day Selection")
         self.dateSel_window.config(bg=bg_center)
@@ -14,6 +17,7 @@ class DateSelWin:
 
         self.logo_image = PhotoImage(file="image/QWQ 2번 로고.png")
 
+        # 본인이 만드신 것이니 주석 안넣겠습니다.
         logo_label = Label(self.dateSel_window, image=self.logo_image, bg=bg_center)
         logo_label.pack(pady=5)
 
@@ -37,16 +41,21 @@ class DateSelWin:
                 button = Button(frame, text=str(level), font=("", "12", "bold"), command=lambda l=level: self.date_seled(l), width=20, height=2, bg=btncol)
                 button.pack(side=TOP)
 
+
     def date_seled(self, level):
         global date_vocab
+        # 사전에 db에 넣어둔 단어db (테이블들 있는데) 불러옴
         db = sql.connect("DB/VocabDB/vocabs.db")
         dbcs = db.cursor()
         date_vocab = dict(dbcs.execute(f"SELECT * FROM day{level}"))
+        # Toplevel이나 Tk()나 상관 없는 것 같네요
         win = Tk()
         if self.sorq == "s":
+            # 이 부분에 창 하나 더 만들어서 플래시 카드를 할건지 단어 리스트를 볼 건지 결정하게 할 겁니다. 우선은 플래시 카드로만
             fC.Flash(win, date_vocab, level)
             win.mainloop()
         else:
+            # 퀴즈 불러옴
             qz.Quiz(win, date_vocab, level, self.id)
             win.mainloop()
         db.commit()
